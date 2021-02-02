262b80b59300cf63ac5c900b38c35d87043ebc00
Jan Bjelicic <jan.bjelicic@sunhill-technologies.com>
2021-02-02 14:27:25 +0100

    Added app icon

--- Hikr/GUI/Views/Profile/UserDataView.swift
+++ Hikr/GUI/Views/Profile/UserDataView.swift
@@ -13,9 +13,8 @@ struct UserDataView: View {
     @StateObject var viewModel: UserDataViewModel
     
     var body: some View {
-        VStack (spacing: 10) {
-            firstName
-            lastName
+        VStack(spacing: 10) {
+            name
             title
             bio
             Spacer()
@@ -24,14 +23,15 @@ struct UserDataView: View {
         .navigationTitle("User information")
     }
     
-    private var firstName: some View {
-        TextField("Enter first name", text: $viewModel.profile.firstName)
-            .textFieldStyle(RoundedBorderTextFieldStyle())
-    }
-
-    private var lastName: some View {
-        TextField("Enter last name", text: $viewModel.profile.lastName)
-            .textFieldStyle(RoundedBorderTextFieldStyle())
+    private var name: some View {
+        VStack {
+            HStack {
+                Label("Name", systemImage: "person.fill")
+                Spacer()
+            }
+            TextField("Enter name", text: $viewModel.profile.name)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+        }
     }
 
     // Make a hidden dropdown
@@ -42,16 +42,15 @@ struct UserDataView: View {
 
     // Multiline
     private var bio: some View {
-        TextField("Bio", text: $viewModel.profile.bio)
-            .textFieldStyle(RoundedBorderTextFieldStyle())
+        TextEditor(text: $viewModel.profile.bio)
+            
     }
 }
 
 struct UserDataView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
-            UserDataView(viewModel: UserDataViewModel(profile: .constant(Profile(firstName: "John",
-                                                                                 lastName: "Doe",
+            UserDataView(viewModel: UserDataViewModel(profile: .constant(Profile(name: "John",
                                                                                  title: "MR",
                                                                                  bio: "I'm a software developer"))))
         }
